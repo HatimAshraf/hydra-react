939f73ffa1a4bebe03ea29a96d4c750205d8a302
Hatim bin Ashraf <hatim.ashraf91@gmail.com>
2025-11-16 22:03:03 +0100

    starting new project dashboard-in-nextjs

--- memory-game/src/App.jsx
+++ memory-game/src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import Card from './components/Card';
 import { Header } from './components/Header';
 const cardValues = [
